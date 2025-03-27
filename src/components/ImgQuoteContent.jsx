03a3ad72acf066cb8a89e8e72d9616592e2c6b34
lalitparashar06 <lalitparashar06@gmail.com>
2025-03-27 19:12:47 +0530

    update

--- src/components/ImgQuoteContent.jsx
+++ src/components/ImgQuoteContent.jsx
@@ -2,9 +2,9 @@ export default function ImgQuoteContent() {
   return (
     <section className="ImgQuoteContent bg-[#E5E4E7]">
       <div className="container">
-        <div className="outer-wrap relative flex items-center w-[100%]">
-          <div className="img-item relative w-[45%]">
-            <div className="inner-contentblue absolute top-[-50px] left-[-50px] items-center flex w-[185px] h-[185px] text-center rounded-[100px] bg-[#fff] border-8 border-[#ED268B] ">
+        <div className="outer-wrap relative flex items-center w-[100%] lg:block  lg:text-center">
+          <div className="img-item relative w-[45%] lg:w-full tablet:w-full phablet:w-full phone:w-full xs:w-full lg:mb-[30px]">
+            <div className="inner-contentblue absolute top-[-50px] left-[-50px] items-center flex w-[185px] h-[185px] text-center rounded-[100px] bg-[#fff] border-8 border-[#ED268B] phone:w-[150px] phone:h-[150px] phone:left-[-19px] xs:w-[150px] xs:h-[150px] xs:left-[-19px] ">
               <div className="inner-outer">
                 <span className="text-wrap text-black text-[32px] font-normal leading-[38px] tracking-[-0.05em] text-center">
                   $20m
@@ -19,8 +19,8 @@ export default function ImgQuoteContent() {
             </div>
             <div className=""></div>
           </div>
-          <div className="content-item w-[55%]">
-            <div className="content-wrap pl-[60px]">
+          <div className="content-item w-[55%] lg:w-full tablet:w-full phablet:w-full phone:w-full xs:w-full">
+            <div className="content-wrap pl-[60px] lg:pl-0 tablet:pl-0 phone:pl-0 xs:pl-0">
               <h3 className="text-black text-[30px] font-bold leading-[36px] tracking-[-0.03em] text-left">
                 Lorem ipsum dolor sit amet consectetur. Ipsum amet in diam
                 vitae.
@@ -41,16 +41,16 @@ export default function ImgQuoteContent() {
                 adipiscing elit, sed do eiusmod tempor incididunt ut labore et
                 dolore magna aliqua. Ut enim ad
               </p>
-              <div className="inner-wrap flex flex-wrap pt-[50px]">
-                <div className="logo-wrap w-[30%] border-r border-black text-left flex items-center">
-                  <div className="inner-icon relative max-w-[135px] h-[34px] z-2 w-full overflow-hidden">
+              <div className="inner-wrap flex flex-wrap pt-[50px] phone:block xs:block">
+                <div className="logo-wrap w-[30%] m-auto border-r border-black text-left flex items-center phone:w-full phone:border-0 phone:m-auto xs:w-full xs:border-0 xs:m-auto">
+                  <div className="inner-icon relative h-[34px] z-2 w-full overflow-hidden ">
                     <img
                       src="/images/sures-icon.svg"
                       className="object-contain"
                     ></img>
                   </div>
                 </div>
-                <div className="inner-content w-[70%] pl-[70px] flex items-center">
+                <div className="inner-content w-[70%] pl-[70px] flex items-center phone:w-full phone:pt-[30px] phone:pl-0 xs:pt-[30px] xs:pl-0">
                   <p className="text-black text-base font-normal leading-6 tracking-normal text-left overflow-hidden">
                     Aaron "Checo" Pacheco, Product Delivery Infrastructure
                     Manager at Acquia
