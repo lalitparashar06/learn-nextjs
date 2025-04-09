927e6b05270c9f9b44d6e64d07f8f1784e5698a4
lalitparashar06 <lalitparashar06@gmail.com>
2025-04-09 18:24:57 +0530

    update chef

---
+++ src/components/SailpointContent.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function Viralone() {
+  return (
+    <section class="sailpointcontent bg-[#FBFBFB]">
+      <div class="container ">
+        <div class="row flex flex-wrap items-center">
+          <div class="col-two w-[45%] lg:w-full fadeInLeft">
+            <div class="content max-w-[450px] lg:max-w-full lg:mb-8">
+              <h2 class="text-black text-[30px] font-bold leading-[32px] tracking-[-0.025em] mb-5 text-left">
+                Headline about analytics
+              </h2>
+              <p class="max-w-[437px] text-black text-left font-normal leading-[24px] mb-5 lg:max-w-full">
+                Lorem ipsum dolor sit dolore amet, consectetur adipiscing set
+                amet elit. Suspendisse eget nulla in urna dictum placerat ac in
+                libero. In nunc erat, pretium a auctor non, commodo non est.
+                Phasellus et hendrerit quam. Nunc consectetur eu dui nec
+                laoreet. Maecenas orci dui, pellentesque vitae elit lacinia,
+                fringilla pellentesque at set amet dolore.
+              </p>
+            </div>
+          </div>
+
+          <div class="col-two w-[50%] lg:w-full fadeInRight">
+            <div class="bg-wrap w-full max-h-[650px]">
+              <img
+                src="/images/sailpoint-laptop copy.png"
+                class="w-full h-full object-cover"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/pages/chefkitchen/index.jsx
@@ -0,0 +1,15 @@
+import Viralone from "@/components/Viralone";
+import SailpointContent from "@/components/SailpointContent";
+import SailpointWireframes from "@/components/SailpointWireframes";
+
+import React from "react";
+
+export default function Index() {
+  return (
+    <>
+      <Viralone />
+      <SailpointContent />
+      <SailpointWireframes />
+    </>
+  );
+}
