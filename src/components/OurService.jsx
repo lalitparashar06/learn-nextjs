43061bc5feeff01a4b46eb0a30f91680895c3a15
lalitparashar06 <lalitparashar06@gmail.com>
2026-02-03 00:24:10 +0530

    update

--- src/components/OurService.jsx
+++ src/components/OurService.jsx
@@ -4,9 +4,9 @@ export default function OurService() {
     <section class="OurService relative  ">
       <div className="container">
         <h2 className="text-center">Our Services</h2>
-        <div className="row relative w-[calc(100%+40px)]  flex flex-wrap mt-[100px]">
-          <div className="col-three w-[calc(33.33%-40px)]  mt-0 mx-[20px] ">
-            <div className="text-wrap px-[46px] py-[60px] border-[1px] border-[antiquewhite] text-center hover:shadow-[0px_20px_30px_0px_rgba(0,0,0,0.25)]">
+        <div className="row relative w-[calc(100%+40px)]  flex flex-wrap mt-[100px] justify-center md:w-full">
+          <div className="col-three w-[calc(33.33%-40px)]  mt-0 mx-[20px] lg:w-[40%]  mdDown:w-[100%]  ">
+            <div className="text-wrap px-[46px]  py-[60px] border-[1px] border-[antiquewhite] text-center hover:shadow-[0px_20px_30px_0px_rgba(0,0,0,0.25)]">
               <h5>Better Future</h5>
               <p className="pt-[20px]">
                 Set have great you male grasses yielding yielding first their to
@@ -19,7 +19,7 @@ export default function OurService() {
               </div>
             </div>
           </div>
-          <div className="col-three w-[calc(33.33%-40px)] mt-0 mx-[20px] ">
+          <div className="col-three w-[calc(33.33%-40px)] mt-0 mx-[20px] lg:w-[40%] mdDown:w-[100%] mdDown:mt-[20px] ">
             <div className="text-wrap px-[46px] py-[60px] border-[1px] border-[antiquewhite] text-center hover:shadow-[0px_20px_30px_0px_rgba(0,0,0,0.25)]">
               <h5>Qualified Trainers</h5>
               <p className="pt-[20px]">
@@ -33,7 +33,7 @@ export default function OurService() {
               </div>
             </div>
           </div>
-          <div className="col-three w-[calc(33.33%-40px)]  mt-0 mx-[20px] ">
+          <div className="col-three w-[calc(33.33%-40px)]   mt-0 mx-[20px] lg:w-[40%] lg:mt-[40px]  mdDown:w-[100%] mdDown:mt-[20px]  ">
             <div className="text-wrap px-[46px] py-[60px]  border-[1px] border-[antiquewhite] text-center hover:shadow-[0px_20px_30px_0px_rgba(0,0,0,0.25)]">
               <h5>Job Oppurtunity</h5>
               <p className="pt-[20px]">
